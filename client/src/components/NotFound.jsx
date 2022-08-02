26bf9568ae426ed8f0ddb28ae0a0ed0b9d496a6d
alen-machado <alen.achado@outlook.com>
2022-08-02 12:38:30 -0300

    final

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import s from '../styles/NotFound.module.css'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className={s.div}>
+      <Link to='/home'>
+        <button>Volver al Home!</button>
+      </Link>
+
+
+    </div>
   )
 }
